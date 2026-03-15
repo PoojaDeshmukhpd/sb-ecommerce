a20dac9a9a978a107e8b9ded64b7f34a72345180
Pooja Deshmukh <poojad96577@gmail.com>
2026-03-16 00:47:14 +0530

    OneToOne: SocialUser is the owner of the relationship, mappedBy will not create a any column, vice versa for both entities can also be happen

--- src/main/java/com/ecommerce/sb_ecomm/socialMediaRelationshipExample/models/SocialProfile.java
+++ src/main/java/com/ecommerce/sb_ecomm/socialMediaRelationshipExample/models/SocialProfile.java
@@ -9,9 +9,17 @@ public class SocialProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /* Seconds Way : if SocialUser is the owner of the relationship
+     "socialProfile" this is the object name coming from SocialProfile
+     Here as like first way it will not create any column of the social_user_id because mappedBy is used here*/
+    @OneToOne(mappedBy = "socialProfile")
+//    @JoinColumn(name = "social_user")
+    private SocialUser socialUser;
 
-    @OneToOne
-    @JoinColumn(name ="social_user") // this will be foreign key, if want to follow naming convention in project then use this annotation
-    // eplicitly enabled goreign key using JoinColumn
-    private SocialUser socialUser; // this will add the one column in db as SOCIAL_USER_ID, if object name is user then column name will be USER_ID
+    // First Way : this will be foreign key, if want to follow naming convention in project then use this annotation
+    // Explicitly enabled goreign key using JoinColumn
+//    @OneToOne
+//    @JoinColumn(name ="social_user")
+//
+//    private SocialUser socialUser; // this will add the one column in db as SOCIAL_USER_ID, if object name is user then column name will be USER_ID
 }
--- src/main/java/com/ecommerce/sb_ecomm/socialMediaRelationshipExample/models/SocialUser.java
+++ src/main/java/com/ecommerce/sb_ecomm/socialMediaRelationshipExample/models/SocialUser.java
@@ -1,9 +1,6 @@
 package com.ecommerce.sb_ecomm.socialMediaRelationshipExample.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class SocialUser {
@@ -11,4 +8,17 @@ public class SocialUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /* Seconds Way : if SocialUser is the owner of the relationship
+     if want to make SocialProfile as owner of the relationship do vice versa*/
+    @OneToOne
+    @JoinColumn(name = "social_profile_id")
+   SocialProfile socialProfile;
+
+    /*First Way : if i only add this annotation in SocialProfile then it will unidirectional
+     but now i have added OneToOne here so it's bidirectional
+     so here socialProfile PK is working as FK in SocialUser and
+     Social User PK key is working as FK in SocialProfile
+     So this is the redundant data this is not good */
+//    @OneToOne
+//    private SocialProfile socialProfile;
 }
